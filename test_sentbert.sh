90327437b4c3807d696a39f3706f020166052de5
Karthik Ganesan <Karthikganesan17@gmail.com>
2022-11-15 02:03:02 -0500

    Create test_sentbert.sh

---
+++ test_sentbert.sh
@@ -0,0 +1,73 @@
+#
+
+
+# from scipy.spatial.distance import cdist
+# --needs to access the actual lib; not sure what goes wrong with container
+export LD_LIBRARY_PATH=/home/air/anaconda3/lib
+
+dataset="fed-turn"
+processor=cuda  # cpu | cuda
+
+scoring=wor  # wr | wor | dial
+# wr: reference-based; turn-level
+# wor: reference-free; turn-level
+# dial: global dialog rating
+
+if [ $# -eq 0 ]; then echo "$0: -d <dataset> -p <processor> -s <scoring>"; exit 2 ; fi
+
+SHORT=d:,p:,s:,h
+LONG=dataset:,processor:,scoring:,help
+OPTS=$(getopt --options $SHORT --longoptions $LONG -- "$@")
+
+eval set -- "$OPTS"
+while [ : ]; do
+    case "$1" in
+	-d | --dataset )
+	    dataset=$2
+	    shift 2
+	    ;;
+	-p | --processor )
+	    processor=$2
+	    shift 2
+	    ;;
+	-s | scoring )
+	    scoring=$2
+	    shift 2
+	    ;;
+	-- )
+	    shift;
+	    break
+	    ;;
+	* | ? )
+	    echo "unexpected option: $1 "
+	    exit 1
+	    ;;
+    esac
+done
+
+if [[ $? -ne 0 ]]; then  exit 1; fi
+
+# coordinates
+echo "dataset:    ${dataset}"
+echo "processor:  ${processor}"
+echo "scoring:    ${scoring}"
+python --version
+echo ""
+# debug="-m pdb"
+
+# score selected system
+python ${debug} compute_sent_${scoring}.py \
+       \
+       --dataset=${dataset} \
+       --device=${processor} \
+       \
+       --am_model_path=../../embedding_models/full_am \
+       --fm_model_path=../../language_models/full_fm # \
+#   | grep 'annotations.Overall' > "${dataset}_${scoring}__results.score"
+#cat  "${dataset}_${scoring}__results.score}"  # show the result on screen
+echo "---------------------"
+
+
+#
+
+
